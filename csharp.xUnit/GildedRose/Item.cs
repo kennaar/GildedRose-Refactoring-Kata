1ed984c117459bb5dde1c75fb691eb00c53d8868
Kennard Vermeiren <kennaar@inuits.eu>
2025-03-17 11:33:53 +0100

    The Quality is never negative

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -1,8 +1,16 @@
-﻿namespace GildedRoseKata;
+﻿using System;
+
+namespace GildedRoseKata;
 
 public class Item
 {
     public string Name { get; set; }
     public int SellIn { get; set; }
-    public int Quality { get; set; }
+
+    private int _quality;
+    public int Quality
+    {
+        get => _quality;
+        set => _quality = Math.Clamp(value, 0, int.MaxValue);
+    }
 }
--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -14,4 +14,19 @@ public class GildedRoseTest
         app.UpdateQuality();
         Assert.Equal("fixme", Items[0].Name);
     }
+    
+    /// <summary>
+    /// @TODO: Is Item.Quality a percentage? Then 100 is the max, and we should clamp to 100
+    /// </summary>
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(100, 100)]
+    [InlineData(-1, 0)]
+    public void TheQualityIsNeverNegative(int quality, int expectedQuality)
+    {
+        Item item = new Item { Name = "foo", SellIn = 0, Quality = quality };
+        
+        Assert.True(item.Quality >= 0);
+        Assert.Equal(expectedQuality, item.Quality);
+    }
 }
