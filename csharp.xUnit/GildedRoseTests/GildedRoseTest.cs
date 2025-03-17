58b0ed9fd750a3197b8b555ae36f40ee0e2a3246
Kennard Vermeiren <kennaar@inuits.eu>
2025-03-17 12:04:55 +0100

    Remove/fix broken tests

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,12 +7,12 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Fact]
-    public void foo()
+    public void TheNameDoesNotChangeAfterUpdatingTheQuality()
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
+        IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+        Assert.Equal("foo", items[0].Name);
     }
     
     /// <summary>
